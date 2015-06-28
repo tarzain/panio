6af95bdb72edd9a59ca55c79b0b6b9e24e20ec8f
Zain Shah <zan2434@gmail.com>
2015-06-27 17:50:41 -0700

    fixed git issues and app now builds for iOS

---
+++ iOS_app/VideoPlayRecord/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  VideoPlayRecord
+//
+//  Created by Abdul Azeem Khan on 5/9/12.
+//  Copyright (c) 2012 DataInvent. All rights reserved.
+//  Happy Coding
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
