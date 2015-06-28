331509e218005a4437374b087b9549b2eeb88692
dieseldeesh <adhishramkumar@yahoo.com>
2015-06-27 18:44:05 -0700

    adding gyro data

--- iOS_app/VideoPlayRecord/RecordVideo.m
+++ iOS_app/VideoPlayRecord/RecordVideo.m
@@ -7,9 +7,9 @@
 //
 
 #import "RecordVideo.h"
+#import <CoreMotion/CoreMotion.h>
 
 @implementation RecordVideo
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -59,7 +59,39 @@
 }
 
 -(void)onTick:(NSTimer *)timer {
-    NSLog(@"The code runs through here");
+    self.motionManager = [[CMMotionManager alloc] init];
+    
+    
+    //Gyroscope
+    if([self.motionManager isGyroAvailable])
+    {
+        /* Start the gyroscope if it is not active already */
+        if([self.motionManager isGyroActive] == NO)
+        {
+            /* Update us 2 times a second */
+            [self.motionManager setGyroUpdateInterval:1.0f / 2.0f];
+            
+            /* Add on a handler block object */
+            
+            /* Receive the gyroscope data on this block */
+            [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue]
+                                            withHandler:^(CMGyroData *gyroData, NSError *error)
+             {
+                 NSString *x = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.x];
+                 self.gyro_xaxis.text = x;
+                 
+                 NSString *y = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.y];
+                 self.gyro_yaxis.text = y;
+                 
+                 NSString *z = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.z];
+                 self.gyro_zaxis.text = z;
+             }];
+        }
+    }
+    else
+    {
+        NSLog(@"Gyroscope not Available!");
+    }
 }
 
 - (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
