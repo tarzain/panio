02e08d5a58a8718c7cc8938effad38368aa174fe
Zain Shah <zan2434@gmail.com>
2015-06-27 23:25:25 -0700

    now actually uploading videos

--- iOS_app/VideoPlayRecord/RecordVideo.m
+++ iOS_app/VideoPlayRecord/RecordVideo.m
@@ -12,6 +12,7 @@
 CMMotionManager *motionManager;
 CMAttitude *referenceAttitude;
 NSMutableArray *gyroDataStream;
+NSMutableData *responseData;
 
 @implementation RecordVideo
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -67,7 +68,6 @@ NSMutableArray *gyroDataStream;
     referenceAttitude = nil;
 //    NSMutableArray *gyroDataStream = [NSMutableArray arrayWithObjects:@[@0,@0,@0], nil];
     gyroDataStream = [[NSMutableArray alloc] init];
-    NSLog(@"Gyroscope Available!");
 
     //Gyroscope
     if([motionManager isGyroAvailable])
@@ -139,6 +139,7 @@ NSMutableArray *gyroDataStream;
     NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
     
     [self dismissModalViewControllerAnimated:NO];
+    NSLog(@"sending videoooo");
     
     // Handle a movie capture
     if (CFStringCompare ((__bridge_retained CFStringRef) mediaType, kUTTypeMovie, 0)
@@ -146,14 +147,25 @@ NSMutableArray *gyroDataStream;
         
         NSString *moviePath = [[info objectForKey:
                                 UIImagePickerControllerMediaURL] path];
+        NSURL *movieURL = [info objectForKey:UIImagePickerControllerMediaURL];
+        NSData *webData = [NSData dataWithContentsOfURL:movieURL];
+//        [self post:webData];
+//        [self uploadvideo:webData];
+        NSURLRequest *urlRequest = [self postRequestWithURL:@"http://ec2-54-187-10-230.us-west-2.compute.amazonaws.com:5000/classify_upload"
+                                                       data:webData
+                                                   fileName:@"clip.mov"];
+        
+        NSURLConnection *uploadConnection =[[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
+        
+        
         if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
             UISaveVideoAtPathToSavedPhotosAlbum (moviePath,self, @selector(video:didFinishSavingWithError:contextInfo:), nil);
         }
         
-        
     }
     [motionManager stopGyroUpdates];
     NSLog( @"%@",[gyroDataStream componentsJoinedByString:@" FINAL, "]);
+    
 }
 
 - (void)video:(NSString*)videoPath didFinishSavingWithError:(NSError*)error contextInfo:(void*)contextInfo
@@ -164,7 +176,154 @@ NSMutableArray *gyroDataStream;
     }else{
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"  delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
         [alert show];
+        
     }
+}
+- (NSData *)generatePostDataForData:(NSData *)uploadData
+{
+    // Generate the post header:
+    NSString *post = [NSString stringWithCString:"--AaB03x\r\nContent-Disposition: form-data; name=\"upload[file]\"; filename=\"somefile\"\r\nContent-Type: application/octet-stream\r\nContent-Transfer-Encoding: binary\r\n\r\n" encoding:NSASCIIStringEncoding];
+    
+    // Get the post header int ASCII format:
+    NSData *postHeaderData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    
+    // Generate the mutable data variable:
+    NSMutableData *postData = [[NSMutableData alloc] initWithLength:[postHeaderData length] ];
+    [postData setData:postHeaderData];
+    
+    // Add the image:
+    [postData appendData: uploadData];
+    
+    // Add the closing boundry:
+    [postData appendData: [@"\r\n--AaB03x--" dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
+    
+    // Return the post data:
+    return postData;
+}
+
+- (void)post:(NSData *)fileData
+{
+    
+    NSLog(@"POSTING");
+    
+    // Generate the postdata:
+    NSData *postData = [self generatePostDataForData: fileData];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    
+    // Setup the request:
+    NSMutableURLRequest *uploadRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://ec2-54-187-10-230.us-west-2.compute.amazonaws.com:5000/classify_upload"] cachePolicy: NSURLRequestReloadIgnoringLocalCacheData timeoutInterval: 1e4 ];
+    [uploadRequest setHTTPMethod:@"POST"];
+    [uploadRequest setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [uploadRequest setValue:@"multipart/form-data; boundary=AaB03x" forHTTPHeaderField:@"Content-Type"];
+    [uploadRequest setHTTPBody:postData];
+    
+    responseData = [[NSMutableData alloc] init];
+    
+    // Execute the reqest:
+    NSURLConnection *conn=[[NSURLConnection alloc] initWithRequest:uploadRequest delegate:self];
+    if (conn)
+    {
+        // Connection succeeded (even if a 404 or other non-200 range was returned).
+        NSLog(@"sucess");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Got Server Response" message:@"Success" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+    else
+    {
+        // Connection failed (cannot reach server).
+        NSLog(@"fail");
+    }
+    
+}
+
+-(void) connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [responseData appendData:data];
+    NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSASCIIStringEncoding];
+    NSLog(@"Got server response %@", responseString);
+//    NSLog(responseString);
+}
+
+- (void)uploadvideo:(NSData *)data{
+    
+//    NSString *strurl=[NSString stringWithFormat:@"%@",];
+    
+//    NSString *url=[[NSString alloc]initWithFormat:@"video_upload.php?user_id=%@&node_id=%@",appDele.UserId,appDele.strProductId];
+    
+    
+//    NSString *urlString =[NSString stringWithFormat:@"%@%@",strurl,url] ;
+//    NSLog(@"%@",urlString);
+    
+//    NSString *encodedString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    // setting up the request object now
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"http://ec2-54-187-10-230.us-west-2.compute.amazonaws.com:5000/classify_upload"]];
+    [request setHTTPMethod:@"POST"];
+    [request setTimeoutInterval:10000000];
+    
+    
+    
+    // NSInputStream *videoStream = [[[NSInputStream alloc] initWithData:data1] autorelease];
+    // [request setHTTPBodyStream:videoStream];
+    
+    NSString *boundary = [NSString stringWithFormat:@"---------------------------14737809831466499882746641449"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    /*
+     now lets create the body of the post
+     */
+    NSMutableData *body = [NSMutableData data];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"filename\"; filename=\"New.mp4\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    // NSLog(@"%@New.jpg",appDelegate.MemberId);
+    [body appendData:[[NSString stringWithFormat:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    //  [body appendData:data1.length];
+//    [body appendData:[NSData dataWithData:data1]];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    // setting the body of the post to the reqeust
+    
+    [request setHTTPBody:body];
+    
+    // now lets make the connection to the web
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",returnString);
+}
+
+-(NSURLRequest *)postRequestWithURL: (NSString *)url
+                               data: (NSData *)data
+                           fileName: (NSString*)fileName
+{
+    
+    // from http://www.cocoadev.com/index.pl?HTTPFileUpload
+    
+    //NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] init];
+    [urlRequest setURL:[NSURL URLWithString:url]];
+//    [urlRequest setURL:url];
+    
+    [urlRequest setHTTPMethod:@"POST"];
+    
+    NSString *myboundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",myboundary];
+    [urlRequest addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    
+    //[urlRequest addValue: [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundry] forHTTPHeaderField:@"Content-Type"];
+    
+    NSMutableData *postData = [NSMutableData data]; //[NSMutableData dataWithCapacity:[data length] + 512];
+    [postData appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", myboundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"imagefile\"; filename=\"%@\"\r\n", fileName]dataUsingEncoding:NSUTF8StringEncoding]];
+    [postData appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postData appendData:[NSData dataWithData:data]];
+    [postData appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", myboundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    [urlRequest setHTTPBody:postData];
+    return urlRequest;
 }
 
 @end
